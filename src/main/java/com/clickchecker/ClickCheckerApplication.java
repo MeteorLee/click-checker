449f2fc5ce0a35dabe9196d3a2a12b6dfad7ae4e
MeteorLee <meteorproject529@gmail.com>
2026-01-07 19:27:34 +0900

    chore: bootstrap spring project

---
+++ src/main/java/com/clickchecker/ClickCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.clickchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickCheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClickCheckerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clickchecker/ClickCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clickchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickCheckerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
