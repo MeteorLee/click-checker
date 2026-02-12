4e4cf8513d7f57af97766283f008ee6018691a3f
MeteorLee <meteorproject529@gmail.com>
2026-02-12 17:09:32 +0900

    refactor: JPAQueryFactory를 Bean 등록 방식 변경

--- src/main/java/com/clickchecker/config/QuerydslConfig.java
+++ src/main/java/com/clickchecker/config/QuerydslConfig.java
@@ -2,18 +2,14 @@ package com.clickchecker.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class QuerydslConfig {
 
-    @PersistenceContext
-    private EntityManager em;
-
     @Bean
-    public JPAQueryFactory jpaQueryFactory() {
-        return new JPAQueryFactory(em);
+    public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+        return new JPAQueryFactory(entityManager);
     }
 }
