7a304620b71cc28b3e866d6238e7358d791f2a8f
MeteorLee <meteorproject529@gmail.com>
2026-03-03 20:53:38 +0900

    test(db): postgres testcontainers 테스트와 h2 테스트 분리

--- src/test/java/com/clickchecker/event/controller/EventQueryControllerPostgresIntegrationTest.java
+++ src/test/java/com/clickchecker/event/controller/EventQueryControllerPostgresIntegrationTest.java
@@ -4,6 +4,7 @@ import com.clickchecker.event.entity.Event;
 import com.clickchecker.event.repository.EventRepository;
 import com.clickchecker.organization.entity.Organization;
 import com.clickchecker.organization.repository.OrganizationRepository;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -23,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Testcontainers
+@Tag("postgres")
 @ActiveProfiles("ci")
 @SpringBootTest
 @AutoConfigureMockMvc
