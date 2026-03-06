b5bacaeeea80e998d81e3ac3a2d11422525b2177
MeteorLee <meteorproject529@gmail.com>
2026-03-06 23:17:26 +0900

    feat(event-user): auditing 추가

--- src/main/java/com/clickchecker/eventuser/entity/EventUser.java
+++ src/main/java/com/clickchecker/eventuser/entity/EventUser.java
@@ -3,6 +3,7 @@ package com.clickchecker.eventuser.entity;
 import com.clickchecker.organization.entity.Organization;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,14 +12,19 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
+import java.time.Instant;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(
         name = "users",
         uniqueConstraints = {
@@ -37,6 +43,14 @@ public class EventUser {
 
     @Column(name = "external_user_id", nullable = false, length = 100)
     private String externalUserId;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private Instant updatedAt;
 
     @Builder
     private EventUser(Organization organization, String externalUserId) {
